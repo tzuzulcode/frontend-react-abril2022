d8968ebab4dfef6e75a5fe9a15fbb2a1e57733cf
Tzuzul Code <mail@tzuzulcode.com>
2022-04-20 21:05:40 -0500

    Reto: Lista de alumnos

---
+++ ejemplo/src/components/Listas.jsx
@@ -0,0 +1,23 @@
+const Listas =()=>{
+
+
+    //Reto: Mostrar datos de los alumnos
+    const alumnos = [
+        {
+            name:"Tzuzul",
+            edad:23
+        },
+        {
+            name:"Eduardo",
+            edad:25
+        },
+        {
+            name:"Claudia",
+            edad:24
+        },
+    ]
+
+    return <div>
+
+    </div>
+}
