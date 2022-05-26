9f3f61a9ec5b3088208d20f95b9cd3e44ad9317a
Tzuzul <mail@tzuzulcode.com>
2022-05-25 21:04:55 -0500

    Configuración de Tailwind y el proyecto

---
+++ ecommerce/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-orange-500 text-5xl font-bold sm:text-red-700 md:text-green-400">Hola</h1>
+      <h2>Hola</h2>
+      <p>Hola</p>
+    </div>
+  );
+}
+
+export default App;
