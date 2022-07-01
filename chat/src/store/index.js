8630b739c049e0a7a0d1044f7f66070414749ef2
Tzuzul <mail@tzuzulcode.com>
2022-06-30 21:12:39 -0500

    Redux

---
+++ chat/src/store/index.js
@@ -0,0 +1,10 @@
+import {configureStore} from '@reduxjs/toolkit'
+import authReducer from '../features/auth'
+
+const store = configureStore({
+    reducer:{
+        auth:authReducer
+    }
+})
+
+export default store
